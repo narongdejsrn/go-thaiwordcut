04fdf8146dfdcb72ba86eb67bbc65ac16a3675eb
Narongdej Sarnsuwan <zenyaith@gmail.com>
2017-11-15 00:01:55 +0700

    Add benchmark

--- wordcut_test.go
+++ wordcut_test.go
@@ -3,6 +3,7 @@ package gothaiwordcut
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 )
 
 func TestPureThaiCut(t *testing.T) {
@@ -17,4 +18,11 @@ func TestMixEnglishThaiCut(t *testing.T) {
 	segmenter.LoadDefaultDict()
 	result := segmenter.Segment("มาลองตัดคำปนภาษา English กันนะ Alright เพื่อน")
 	assert.Equal(t, []string{"มา", "ลอง", "ตัด", "คำ", "ปน", "ภาษา", "English", "กัน", "นะ", "Alright", "เพื่อน"}, result)
+}
+
+func BenchmarkWordcut(b *testing.B) {
+	segmenter := Wordcut()
+	segmenter.LoadDefaultDict()
+	dat, _ := ioutil.ReadFile("./dict/benchmark_text.txt")
+	segmenter.Segment(string(dat))
 }
